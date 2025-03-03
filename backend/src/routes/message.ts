4ea30235786f40618cce4cc668a39a7f555e8857
Dabs <paulandrei.ardiente-23@cpu.edu.ph>
2025-03-03 23:05:46 +0800

    final?

--- backend/src/routes/message.ts
+++ backend/src/routes/message.ts
@@ -1,17 +1,36 @@
-import { Router } from 'express'
+import { Router, Request, Response, NextFunction} from 'express'
 
 const router = Router()
 
-router.get('/', (req, res) => {
-  res.send('YOoooooo')
+
+router.get('/', (req, res, next) => {
+  try {
+    res.status(200).json({ message: 'YOoooooo' })
+  } catch (error) {
+    next(error)
+  }
 })
 
-router.post('/', (req, res) => {
-  res.send('I am in post')
+
+router.post('/', (req, res, next) => {
+  try {
+    res.status(201).json({ message: 'I am in POST', receivedData: req.body })
+  } catch (error) {
+    next(error)
+  }
 })
 
-router.put('/', (req, res) => {
-  res.send('I am in put')
+router.put('/', (req, res, next) => {
+  try {
+    res.status(200).json({ message: 'I am in PUT', updatedData: req.body })
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.error(err.stack)
+  res.status(500).json({ error: 'Something went wrong', details: err.message })
 })
 
 export default router
