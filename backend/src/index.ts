8bf56f7eb54704537c2a8e686e9763199d005545
Dabs <paulandrei.ardiente-23@cpu.edu.ph>
2025-03-04 17:30:34 +0800

    [Paul] fixed and issue where I am using 2 app.listen on the port

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,10 @@
 import express from 'express'
-import cors from "cors"
+import cors from 'cors'
 import messageRoute from './routes/message'
 
 const app = express()
 const PORT = 3000
-app.listen(PORT)
+// app.listen(PORT)
 
 app.use(cors())
 app.use(express.json())
@@ -13,10 +13,8 @@ app.get('/', (req, res) => {
   res.send('hello Paul')
 })
 
-app.use("/messages",messageRoute)
-
+app.use('/messages', messageRoute)
 
 app.listen(PORT, () => {
   console.log(`Server can be found at http://localhost:${PORT}`)
 })
-
